04ce814257b40a3c06c5abaf2f207952698fe1e7
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-07-14 09:33:51 -0400

    Update windows_server_setup.ps1
    Added Enable-RDP and Disable-RDP functions

--- scripting/windows_server_setup.ps1
+++ scripting/windows_server_setup.ps1
@@ -4,31 +4,59 @@
 
 .NOTES
     This script has been created\curated by Matthew Kolakowski (m@kolakowski.us)
-
-
 #>
-
-
 
 
 <#
 .Source
-Credit to Joe Harris from https://serverspace.us/support/help/disable-enhanced-security-windows-server/
+    Credit to Maruo Huc https://pureinfotech.com/enable-remote-desktop-powershell-windows-10/
 
 .SYNOPSIS
-Disable Enhanced Security in Internet Explorer on Windows Server 2019
+    Enables Remote Desktop Protocal (RDP) and enables firewall rules on Windows Server 2019
 
 .EXAMPLE
-Disable-IEESC
-Run this command in powershell and the acl will be applied
+    Enable-RDP
+        Run this command in powershell and the acl will be applied
+#>
+function Enable-RDP {
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+}
+
+<#
+.Source
+    Credit to Maruo Huc https://pureinfotech.com/enable-remote-desktop-powershell-windows-10/
+
+.SYNOPSIS
+    Disable Remote Desktop Protocal (RDP) and disables firewall rules on Windows Server 2019
+
+.EXAMPLE
+    Disable-RDP
+        Run this command in powershell and the acl will be applied
+#>
+function Disable-RDP {
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 1
+    Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
+}
+
+<#
+.Source
+    Credit to Joe Harris from https://serverspace.us/support/help/disable-enhanced-security-windows-server/
+
+.SYNOPSIS
+        Disable Enhanced Security in Internet Explorer on Windows Server 2019
+
+.EXAMPLE
+    Disable-IEESC
+        Run this command in powershell and the acl will be applied
 #>
 function Disable-IEESC {
-$AdminKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-Stop-Process -Name Explorer
-Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+    $AdminKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
 }
 
 
