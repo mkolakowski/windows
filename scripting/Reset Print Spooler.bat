b7f0f5815cd99841108d6d0a8cfb6e827b978938
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-07-15 10:26:23 -0400

    Update Reset Print Spooler.bat
    Added Logging

--- scripting/Reset Print Spooler.bat
+++ scripting/Reset Print Spooler.bat
@@ -1,21 +1,22 @@
+echo off
+rem generating timestam for log file
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
-set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
-echo datestamp: "%datestamp%"
-echo timestamp: "%timestamp%"
-echo fullstamp: "%fullstamp%"
 
+set "printspoolresetlog=C:\Logs\reset-print-spooler-%fullstamp%.txt"
 
-net stop spooler
+rem Creating Log folder
+mkdir C:\Logs\
+
 echo Run me as elevated
 
-mkdir C:\scripting\reset-print-spooler-%fullstamp%.txt
+net stop spooler > %printspoolresetlog% | type %printspoolresetlog%
 
-del %systemroot%\System32\spool\printers\* /Q /F /S
+del %systemroot%\System32\spool\printers\* /Q /F /S > %printspoolresetlog% | type %printspoolresetlog%
  
-net start spooler
+net start spooler > %printspoolresetlog% | type %printspoolresetlog%
 
 pause
