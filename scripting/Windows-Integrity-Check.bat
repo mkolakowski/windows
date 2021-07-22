4cb309ecd749baa8f0c02d7df4e014b9fe2a9188
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-07-22 14:02:08 -0400

    Added needed logging to script

--- scripting/Windows-Integrity-Check.bat
+++ scripting/Windows-Integrity-Check.bat
@@ -1,3 +1,46 @@
-sfc /scannow
-DISM.exe /Online /Cleanup-Image /Scanhealth
-DISM.exe /Online /Cleanup-Image /Restorehealth
+echo off
+
+echo Run me as elevated
+
+rem generating timestam for log file
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+set "winIntegrityCheckLog=C:\Logs\reset-print-spooler-%fullstamp%.txt"
+
+rem Creating Log folder
+mkdir C:\Logs\ >> %winIntegrityCheckLog% 2>&1
+
+echo Script ran by %USERNAME% >> %winIntegrityCheckLog% 2>&1
+
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+echo %HH%-%Min%-%Sec% - sfc /scannow  >> %winIntegrityCheckLog% 2>&1
+sfc /scannow  >> %winIntegrityCheckLog% 2>&1
+
+
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+echo %HH%-%Min%-%Sec% - DISM.exe /Online /Cleanup-Image /Scanhealth >> %winIntegrityCheckLog% 2>&1
+DISM.exe /Online /Cleanup-Image /Scanhealth  >> %winIntegrityCheckLog% 2>&1
+
+
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+echo %HH%-%Min%-%Sec% - DISM.exe /Online /Cleanup-Image /Restorehealth  >> %winIntegrityCheckLog% 2>&1
+DISM.exe /Online /Cleanup-Image /Restorehealth  >> %winIntegrityCheckLog% 2>&1
+
+
+rem generating timestamp for log file
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo %HH%-%Min%-%Sec% - Script Complete  >> %winIntegrityCheckLog% 2>&1
+
+
+echo Read log file here: %winIntegrityCheckLog%
+
+pause
