e541108031c13022a33bf04ecb0d33692efb2ce2
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2021-10-11 11:25:17 -0400

    Create Windows-Integrity.ps1

---
+++ scripting/Windows-Integrity.ps1
@@ -0,0 +1,79 @@
+echo off
+# This script has been created\curated by Matthew Kolakowski (m@kolakowski.us)
+# https://github.com/mkolakowski
+# This script does not have any warranty and must be ran with extreem caution, by continuing to run this you are acceptintg of the terms
+# Ensure all data is backed up as there is no gaurentee that you will not loose data when running this script
+# Please ensure this script is ran as admin before contunuing
+pause
+
+$CurrentDate = (get-date).toString("yyyy-MM-dd_hh-mm-ss") # Sets Current Date for timestamping
+$winIntegrityCheckLog = "C:\Logs\Windows-Integrity-Check-$CurrentDate.txt"
+
+
+param([switch]$Elevated)
+
+function Generate-Current-Date {
+    $CurrentDate = (get-date).toString("yyyy-MM-dd hh-mm-ss tt") # Sets Current Date for timestamping
+}
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+        #You may remove the -noexit switch if the terminal should automatically close when the script finishes.
+    }
+    exit
+}
+
+function Create-Log-File {
+    mkdir C:\Logs\
+    echo Read log file here: %winIntegrityCheckLog%
+    echo This script can take quite a bit of time
+    echo Read log file here: $winIntegrityCheckLog
+    Write-To-Log -appendToLog "Creating Log" 
+}
+
+function Write-To-Log ($appendToLog) {
+    Generate-Current-Date
+    $CurrentDate + " ---------- " + $appendToLog | Out-File -Append -FilePath $winIntegrityCheckLog
+}
+
+function Copy-Delete-CBS-Log {
+      Write-To-Log -appendToLog "Script ran by $env:UserName" 
+      Copy-Item -Path "C:\Windows\Logs\CBS\CBS.log" -Destination "C:\Logs\Windows-Integrity-Check-CBS_Log-$CurrentDate.txt" -PassThru
+      
+      Write-To-Log -appendToLog "Deleting CBS Log" 
+      Remove-Item C:\Windows\Logs\CBS\CBS.log | Out-File -Append -FilePath $winIntegrityCheckLog
+}
+
+function Run-SFC-Scan {
+      Write-To-Log -appendToLog "Starting SFC Scan" 
+      sfc /scannow | Out-File -Append -FilePath $winIntegrityCheckLog
+}
+
+function Run-DISM-Scans {
+      Write-To-Log -appendToLog "Starting DISM Health Scan" 
+      Write-To-Log -appendToLog "DISM.exe /Online /Cleanup-Image /Scanhealth" 
+      DISM.exe /Online /Cleanup-Image /Scanhealth | Out-File -Append -FilePath $winIntegrityCheckLog
+            
+      Write-To-Log -appendToLog "Starting Starting Health Restore" 
+      Write-To-Log -appendToLog "DISM.exe /Online /Cleanup-Image /Restorehealth" 
+      DISM.exe /Online /Cleanup-Image /Restorehealth | Out-File -Append -FilePath $winIntegrityCheckLog
+}
+
+#Calling Functions
+  Test-Admin
+  Create-Log-File
+  Copy-Delete-CBS-Log
+  Run-SFC-Scan
+  Run-SFC-Scan
+  Run-DISM-Scans
+  Write-To-Log -appendToLog "Script Completed" 
+
+pause
