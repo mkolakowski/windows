041c1d59e4d72f61765c7a03bf39c67c84554695
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2022-04-19 08:42:07 -0400

    Create Windows-RDS-Profile-Disk-Space-Check.ps1

---
+++ scripting/Windows-RDS-Profile-Disk-Space-Check.ps1
@@ -0,0 +1,16 @@
+#$DisksInWarning = @()
+
+# Modify this vatiable to how much free space is needed
+$PercentFree = 0.20
+
+$VHDS = get-disk | Where-Object {$_.Location -match "VHD"}
+foreach($VHD in $VHDS){
+  $FilePath = [io.path]::GetFileNameWithoutExtension("$($VHD.Location)")
+  #$SIDObject = New-Object System.Security.Principal.SecurityIdentifier ($FilePath) 
+  #$Username = $SIDObject.Translate([System.Security.Principal.NTAccount])
+  $Volume = $VHD | Get-Partition | Get-Volume
+  if($Volume.SizeRemaining -lt $volume.Size * $PercentFree ){ 
+    $PercentFreeOut = $PercentFree * 100
+    Write-Output "$($Username.Value) UPD Less than $PercentFreeOut% remaining. Path: $($VHD.Location)"
+  }
+}
