7e8b0aeee94e7a4e89f7abe3ac9e295040fcce17
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2022-04-19 08:43:33 -0400

    Update Windows-RDS-Profile-Disk-Space-Check.ps1

--- scripting/Windows-RDS-Profile-Disk-Space-Check.ps1
+++ scripting/Windows-RDS-Profile-Disk-Space-Check.ps1
@@ -1,3 +1,6 @@
+# Modified from 
+# https://www.cyberdrain.com/monitoring-with-powershell-monitoring-rds-upd-size/
+
 #$DisksInWarning = @()
 
 # Modify this vatiable to how much free space is needed
@@ -5,7 +8,7 @@ $PercentFree = 0.20
 
 $VHDS = get-disk | Where-Object {$_.Location -match "VHD"}
 foreach($VHD in $VHDS){
-  $FilePath = [io.path]::GetFileNameWithoutExtension("$($VHD.Location)")
+  #$FilePath = [io.path]::GetFileNameWithoutExtension("$($VHD.Location)")
   #$SIDObject = New-Object System.Security.Principal.SecurityIdentifier ($FilePath) 
   #$Username = $SIDObject.Translate([System.Security.Principal.NTAccount])
   $Volume = $VHD | Get-Partition | Get-Volume
