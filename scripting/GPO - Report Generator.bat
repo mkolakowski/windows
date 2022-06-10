5a59ed305b133b645189244af89b830318baa936
mkolakowski <31713098+mkolakowski@users.noreply.github.com>
2022-06-10 10:56:42 -0400

    Create GPO - Report Generator.bat

---
+++ scripting/GPO - Report Generator.bat
@@ -0,0 +1,25 @@
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%%MM%%DD%_%HH%%Min%%Sec%"
+
+mkdir C:\InTrust\GPOreport
+
+set logfile=C:\InTrust\GPOreport\GPOreport-%fullstamp%.txt
+
+rem gpupdate >> %logfile%
+echo   >> %logfile%
+
+echo ------------------------------------------------------------------------------------------------- >> %logfile%
+
+gpresult /r /scope:computer >>%logfile% >> %logfile%
+
+echo   >> %logfile%
+
+echo ------------------------------------------------------------------------------------------------- >> %logfile%
+
+gpresult /r /scope:user >>%logfile% >> %logfile%
+
+%logfile%
